9d436512aa2c2e6db36970d91418e69266347bac
Tani <camel.tn@gmail.com>
2020-07-13 20:05:24 +0900

    :fire: test source

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -56,45 +56,4 @@ class Auth extends MY_Auth
         }
     }
 
-    public function forgotpass()
-    {
-        if (!$this->post || !$this->post['email']) {
-            return $this->json(['result' => false, 'message' => 'メール入力してください', 'post' => $this->post]);
-        }
-
-        $row = $this->user_model->getRow(['email' => $this->post['email']]);
-
-        if (!$row || !isset($row->password)) {
-            return $this->json(['result' => false, 'message' => 'ユーザーが存在しません']);
-        }
-
-        $this->load->library('email');
-        $this->email->initialize([
-            'smtp_host' => env('SMTP_HOST'),
-            'smtp_user' => env('SMTP_USER'),
-            'smtp_pass' => env('SMTP_PASS'),
-            // 'smtp_port' => env('SMTP_PORT'),
-            // 'smtp_crypto' => env('SMTP_CRYPTO'),
-            'protocol' => 'smtp',
-            'wordwrap' => false,
-        ]);
-
-        $this->email->from(env('SMTP_USER'));
-        $this->email->to($row->email);
-        $this->email->bcc(env('SMTP_USER'));
-
-        $this->email->subject('パスワード送信');
-
-        $msg = 'パスワード: ';
-        $msg .= $row->password;
-        $this->email->message($msg);
-
-        $this->email->set_wordwrap(false);
-
-        if ($this->email->send()) {
-            return $this->json(['result' => true, 'message' => 'メール送信しました。']);
-        } else {
-            return $this->json(['result' => false, 'message' => 'メール送信失敗しました。']);
-        }
-    }
 }
