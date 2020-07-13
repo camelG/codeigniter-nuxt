9aaff60e3c31a271d9528295b17a0feddfe0d778
Tani <camel.tn@gmail.com>
2020-07-13 20:03:30 +0900

    update hooks

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+$hook['pre_system'] = function () {
+    try {
+        $dotenv = Dotenv\Dotenv::createImmutable(APPPATH . '../');
+        $dotenv->load();
+    } catch (Exception $e) {
+        // print_r($e);exit;
+    }
+
+    function env(string $key, $default = null)
+    {
+        $value = getenv($key);
+        return $value ?? $default;
+    }
+};
