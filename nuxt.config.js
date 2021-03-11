825c12cf285b7f514848a51be71c1cf76f6c4365
Tani <camel.tn@gmail.com>
2021-03-11 20:32:05 +0900

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,6 @@
 const pkg = require("./package");
 
 module.exports = {
-  mode: "universal",
-  /*
-   ** Headers of the page
-   */
   head: {
     title: pkg.name,
     meta: [
@@ -14,45 +10,20 @@ module.exports = {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-
-  /*
-   ** Customize the progress-bar color
-   */
   loading: { color: "#fff" },
-
-  /*
-   ** Global CSS
-   */
   css: [],
-
-  /*
-   ** Plugins to load before mounting the App
-   */
   plugins: [],
-
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://github.com/nuxt-community/axios-module#usage
-    "@nuxtjs/axios",
-    // Doc: https://bootstrap-vue.js.org/docs/
-    "bootstrap-vue/nuxt"
-  ],
-  /*
-   ** Axios module configuration
-   */
+  modules: ["@nuxtjs/axios", "bootstrap-vue/nuxt"],
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
-
-  /*
-   ** Build configuration
-   */
   build: {
     /*
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  bootstrapVue: {
+    icons: false
   }
 };
